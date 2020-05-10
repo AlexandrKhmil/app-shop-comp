bfeae07e1514d4579ea9795fa6079732da33af93
Alex <oleksandr.khmil2@gmail.com>
2020-05-10 22:23:16 +0300

    forget build

---
+++ client/build/precache-manifest.f7ae8930d327ff84051348bbb3eff75a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92abf7e9c54b96f52fee6d788cc8b9e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8909ce187d48b5c191a3",
+    "url": "/static/css/main.80c26998.chunk.css"
+  },
+  {
+    "revision": "dd6ba782514e427a1f67",
+    "url": "/static/js/2.1696ad2a.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.1696ad2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8909ce187d48b5c191a3",
+    "url": "/static/js/main.b49f92a8.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "d694bd070f7fdf4d21d26a31130ef510",
+    "url": "/static/media/reload.d694bd07.svg"
+  },
+  {
+    "revision": "22e1fa41e75e0be3be11f708200d5155",
+    "url": "/static/media/shopping-cart.22e1fa41.svg"
+  },
+  {
+    "revision": "bbb7ece818f3e9db0d35716fcfc47147",
+    "url": "/static/media/star.bbb7ece8.svg"
+  },
+  {
+    "revision": "af746962879515ec4f0b47fe04f0e1fe",
+    "url": "/static/media/starGray.af746962.svg"
+  }
+]);
