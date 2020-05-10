5b5a8be321cf735d927326732fc103985b3099e8
Alex <oleksandr.khmil2@gmail.com>
2020-05-10 22:12:23 +0300

    pre deploy

--- connection.js
+++ connection.js
@@ -1,4 +1,6 @@
 const pgp = require('pg-promise')();
-const db = pgp(require('config').get('db'));
+pgp.pg.defaults.ssl = process.env.DATABASE_URL && true;
+pgp.pg.defaults.poolSize = 10;
+const db = pgp(process.env.DATABASE_URL || require('config').get('db'));
 
 module.exports = db;
